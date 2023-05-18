1fe3c5bf00a41ca1d19e3238d6defde6766eea69
Vyacheslav Morev <gromd96@gmail.com>
2023-05-18 03:55:01 +0300

    create react app

---
+++ src/components/Card.js
@@ -0,0 +1,28 @@
+function Card(props) {
+
+  function handleClick(evt) {
+    props.onCardClick(evt);
+  }
+
+  return (
+    <section className="cards" aria-label="подборка фотографий интересных мест мира">
+      <ul className="cards__list">
+        {props.card.map((card) => ( 
+          <li className="card" key={card['_id']}>
+            <img className="card__image" src={card.link} alt={card.name} onClick={handleClick} />
+            <button className="card__delete-button card__delete-button_disabled" type="button"></button>
+            <div className="card__content">
+              <h2 className="card__text">{card.name}</h2>
+              <div className="card__place-like">
+                <button className="card__like" type="button"></button>
+                <p className="card__like-counter">{card.likes.length}</p>
+              </div>
+            </div>
+          </li> 
+        ))}
+      </ul>
+    </section>
+  )
+}
+
+export default Card;
---
+++ src/components/Main.js
@@ -0,0 +1,58 @@
+import { api } from "../utils/api.js";
+import React from "react";
+import Card from "./Card.js";
+
+
+function Main(props) {
+  
+  const [userName, setUserName] = React.useState('');
+  const [userDescription, setUserDescription] = React.useState('');
+  const [userAvatar, setUserAvatar] = React.useState('');
+  const [cards, setCards] = React.useState([]);
+
+  React.useEffect(() => {
+    Promise.all([
+      api.getProfileContent({
+        name: userName,
+        about: userDescription,
+        avatar: userAvatar,
+      }),
+      api.getInitialCards()
+    ])
+      .then(([info, cards]) => {
+        setUserName(info.name);
+        setUserDescription(info.about);
+        setUserAvatar(info.avatar);
+        setCards(cards);
+      })
+      .catch((err) => console.log(err));
+
+  }, [userName, userDescription, userAvatar])
+
+  return (
+    <main className="content">
+        <section className="profile">
+          <div className="profile__card">
+            <div className="profile__place-avatar">
+              <img alt="фотография профиля" src={userAvatar} className="profile__avatar" />
+              <button className="profile__avatar-button" type="button" onClick={props.onEditAvatar}></button>
+            </div>
+            <div className="profile__info">
+              <div className="profile__content">
+                <h1 className="profile__title">{userName}</h1>
+                <button className="profile__edit-button" type="button" onClick={props.onEditProfile}></button>
+              </div>
+              <p className="profile__subtitle">{userDescription}</p>
+            </div>
+          </div>
+          <button className="profile__add-button" type="button" onClick={props.onAddPlace}></button>
+        </section>
+        <Card 
+          card={cards}
+          onCardClick={props.onCardClick}
+        />
+      </main>
+  )
+}
+
+export default Main;
