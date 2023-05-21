d208cd5aec335ba1bd4c2ec8d5f6fec5b076508d
Vyacheslav Morev <gromd96@gmail.com>
2023-05-21 21:56:26 +0300

    add fix after review

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,24 +4,22 @@ function Card(props) {
   }
 
   return (
-    <>
-      <li className="card">
-        <img
-          className="card__image"
-          src={props.card.link}
-          alt={props.card.name}
-          onClick={handleClick}
-        />
-        <button className="card__delete-button card__delete-button_disabled" type="button"></button>
-        <div className="card__content">
-          <h2 className="card__text">{props.card.name}</h2>
-          <div className="card__place-like">
-            <button className="card__like" type="button"></button>
-            <p className="card__like-counter">{props.card.likes.length}</p>
-          </div>
+    <li className="card">
+      <img
+        className="card__image"
+        src={props.card.link}
+        alt={props.card.name}
+        onClick={handleClick}
+      />
+      <button className="card__delete-button card__delete-button_disabled" type="button"></button>
+      <div className="card__content">
+        <h2 className="card__text">{props.card.name}</h2>
+        <div className="card__place-like">
+          <button className="card__like" type="button"></button>
+          <p className="card__like-counter">{props.card.likes.length}</p>
         </div>
-      </li>
-    </>
+      </div>
+    </li>
   );
 }
 
